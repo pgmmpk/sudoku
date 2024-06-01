cb5454dcdb64b59228bde40ecd04deb890917fb2
Mike Kroutikov <pgmmpk@gmail.com>
2024-06-01 07:43:08 -0400

    polish ui/ux

--- src/routes/Settings.svelte
+++ src/routes/Settings.svelte
@@ -41,7 +41,7 @@
         <label class="inline-flex items-center cursor-pointer">
             <input type="checkbox" value="" bind:checked={settings.vibrate} class="sr-only peer">
             <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-none dark:peer-focus:ring-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-500"></div>
-            <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-700">Vibrate</span>
+            <span class="ms-4 font-medium text-gray-900 dark:text-gray-700">Vibrate</span>
         </label>
     </div>
     <div class="grow"></div>
